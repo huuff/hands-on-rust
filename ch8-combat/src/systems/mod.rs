c6249965db166a05b3a77188a791fc35b99327eb
Haf <haf@protonmail.ch>
2023-02-14 07:34:03 +0100

    removed collisions system

--- ch8-combat/src/systems/mod.rs
+++ ch8-combat/src/systems/mod.rs
@@ -1,7 +1,6 @@
 mod player_input;
 mod map_render;
 mod entity_render;
-mod collisions;
 mod random_move;
 mod end_turn;
 mod movement;
@@ -26,7 +25,6 @@ pub fn build_player_scheduler() -> Schedule {
     Schedule::builder()
         .add_system(movement::movement_system())
         .flush()
-        .add_system(collisions::collisions_system())
         .flush()
         .add_system(map_render::map_render_system())
         .add_system(entity_render::entity_render_system())
@@ -41,7 +39,6 @@ pub fn build_monster_scheduler() -> Schedule {
         .flush()
         .add_system(movement::movement_system())
         .flush()
-        .add_system(collisions::collisions_system())
         .flush()
         .add_system(map_render::map_render_system())
         .add_system(entity_render::entity_render_system())
