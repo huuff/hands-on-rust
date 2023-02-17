0d0a4e3c69c1995c9144ac6d0bb02561ca0c5afc
Haf <haf@protonmail.ch>
2023-02-17 07:44:30 +0100

    starting ch10

---
+++ ch10-fov/src/spawner.rs
@@ -0,0 +1,65 @@
+use crate::prelude::*;
+
+pub fn spawn_player(ecs: &mut World, pos: Point) {
+    ecs.push(
+        (
+            Player,
+            pos,
+            Render {
+                color: ColorPair::new(WHITE, BLACK),
+                glyph: to_cp437('@'),
+            },
+            Health { current: 10, max: 10},
+        )
+    );
+}
+
+type Monster = (i32, String, FontCharType);
+
+fn goblin() -> Monster {
+    (1, "Goblin".to_string(), to_cp437('g'))
+}
+
+fn orc() -> Monster {
+    (2, "Orc".to_string(), to_cp437('o'))
+}
+
+pub fn spawn_monster(
+    ecs: &mut World,
+    rng: &mut RandomNumberGenerator,
+    pos: Point,
+) {
+    let (hp, name, glyph) = match rng.roll_dice(1, 10) {
+        1..=8 => goblin(),
+        _ => orc(),
+    };
+
+    ecs.push(
+        (
+            Enemy,
+            pos,
+            Render {
+                color: ColorPair::new(WHITE, BLACK),
+                glyph,
+            },
+            Health { current: hp, max: hp },
+            Name(name),
+            ChasingPlayer {},
+        )      
+    );
+}
+
+pub fn spawn_amulet_of_yala(ecs: &mut World, pos: Point) {
+    ecs.push(
+        (
+            Item,
+            AmuletOfYala,
+            pos,
+            Render {
+                color: ColorPair::new(WHITE, BLACK),
+                glyph: to_cp437('|'),
+            },
+            Name("Amulet of Yala".to_string()),
+        )
+    );
+}
