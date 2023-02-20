d650560c800c632950ec7160d022cb24b8d64e53
Haf <haf@protonmail.ch>
2023-02-20 07:49:12 +0100

    randomly choosing a map architect

--- ch11-maps/src/map_builder/mod.rs
+++ ch11-maps/src/map_builder/mod.rs
@@ -144,7 +144,11 @@ impl MapBuilder {
     }
 
     pub fn new(rng: &mut RandomNumberGenerator) -> Self {
-        let mut architect = DrunkardsWalkArchitect {};
+        let mut architect: Box<dyn MapArchitect> = match rng.range(0, 3) {
+            0 => Box::new(DrunkardsWalkArchitect {}),
+            1 => Box::new(RoomsArchitect {}),
+            _ => Box::new(CellularAutomataArchitect {})
+        };
         architect.new(rng)
     }
 }
