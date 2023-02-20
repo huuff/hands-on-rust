008d69006ffb2ff50d270079803e782d340058d5
Haf <haf@protonmail.ch>
2023-02-20 06:22:01 +0100

    updating fov

--- ch10-fov/src/systems/movement.rs
+++ ch10-fov/src/systems/movement.rs
@@ -1,6 +1,7 @@
 use crate::prelude::*;
 
 #[system(for_each)]
+#[read_component(FieldOfView)]
 #[read_component(Player)]
 pub fn movement(
     entity: &Entity,
@@ -9,14 +10,19 @@ pub fn movement(
     #[resource] camera: &mut Camera,
     ecs: &mut SubWorld,
     commands: &mut CommandBuffer,
-    ) {
+) {
     if map.can_enter_tile(wants_move.destination) {
         commands.add_component(wants_move.entity, wants_move.destination);
 
-        if ecs.entry_ref(wants_move.entity)
-                .unwrap()
-                .get_component::<Player>().is_ok() {
-           camera.on_player_move(wants_move.destination); 
+        if let Ok(entry) = ecs.entry_ref(wants_move.entity) {
+            if let Ok(fov) = entry.get_component::<FieldOfView>() {
+                commands.add_component(wants_move.entity, fov.clone_dirty());
+            }
+            if ecs.entry_ref(wants_move.entity)
+                    .unwrap()
+                    .get_component::<Player>().is_ok() {
+               camera.on_player_move(wants_move.destination); 
+            }
         }
     }
     commands.remove(*entity);
