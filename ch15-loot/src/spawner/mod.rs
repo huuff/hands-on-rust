82bd47cf9395f1e773b2c371df0875e430c3c635
Haf <haf@protonmail.ch>
2023-02-23 07:19:41 +0100

    spawning templates

--- ch15-loot/src/spawner/mod.rs
+++ ch15-loot/src/spawner/mod.rs
@@ -1,6 +1,7 @@
 mod template;
 
 use crate::prelude::*;
+use crate::spawner::template::*;
 
 pub fn spawn_player(ecs: &mut World, pos: Point) {
     ecs.push(
@@ -19,51 +20,14 @@ pub fn spawn_player(ecs: &mut World, pos: Point) {
 
 type Monster = (i32, String, FontCharType);
 
-fn goblin() -> Monster {
-    (1, "Goblin".to_string(), to_cp437('g'))
-}
-
-fn orc() -> Monster {
-    (2, "Orc".to_string(), to_cp437('o'))
-}
-
-pub fn spawn_monster(
+pub fn spawn_level(
     ecs: &mut World,
     rng: &mut RandomNumberGenerator,
-    pos: Point,
+    level: usize,
+    spawn_points: &[Point],
 ) {
-    let (hp, name, glyph) = match rng.roll_dice(1, 10) {
-        1..=8 => goblin(),
-        _ => orc(),
-    };
-
-    ecs.push(
-        (
-            Enemy,
-            pos,
-            Render {
-                color: ColorPair::new(WHITE, BLACK),
-                glyph,
-            },
-            Health { current: hp, max: hp },
-            Name(name),
-            ChasingPlayer {},
-            FieldOfView::new(6),
-        )      
-    );
-}
-
-pub fn spawn_entity(
-    ecs: &mut World,
-    rng: &mut RandomNumberGenerator,
-    pos: Point,
-) {
-    let roll = rng.roll_dice(1, 6);
-    match roll {
-        1 => spawn_healing_potion(ecs, pos),
-        2 => spawn_magic_mapper(ecs, pos),
-        _ => spawn_monster(ecs, rng, pos),
-    }
+    let template = Templates::load();
+    template.spawn_entities(ecs, rng, level, spawn_points);
 }
 
 pub fn spawn_amulet_of_yala(ecs: &mut World, pos: Point) {
@@ -79,30 +43,4 @@ pub fn spawn_amulet_of_yala(ecs: &mut World, pos: Point) {
             Name("Amulet of Yala".to_string()),
         )
     );
-}
-
-pub fn spawn_healing_potion(ecs: &mut World, pos: Point) {
-    ecs.push((
-        Item,
-        pos,
-        Render {
-            color: ColorPair::new(WHITE, BLACK),
-            glyph: to_cp437('!'),
-        },
-        Name("Healing Potion".to_string()),
-        ProvidesHealing { amount: 6 },
-    ));
-}
-
-pub fn spawn_magic_mapper(ecs: &mut World, pos: Point) {
-    ecs.push((
-        Item,
-        pos,
-        Render {
-            color: ColorPair::new(WHITE, BLACK),
-            glyph: to_cp437('{'),
-        },
-        Name("Dungeon Map".to_string()),
-        ProvidesDungeonMap {},
-    ));
 }
